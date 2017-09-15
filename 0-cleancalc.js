2fd7dd5c83b5428c1e423464123365c2544dba87
skypto <awuahkwabena@gmail.com>
2017-09-15 17:14:22 +0000

    Update 0-cleancalc.js

--- 0-cleancalc.js
+++ 0-cleancalc.js
@@ -5,8 +5,8 @@
 //Cleancalc
 
 ```javascript
+```javascript
 var lastResult = 0000;
-
 function add(arg1, arg2) {
 	return arg1 + arg2;
 };
@@ -22,9 +22,8 @@ function multiply(arg1, arg2) {
 function divide(arg1, arg2) {
 	return arg1 / arg2;
 };
-```
 
 lastResult = add(2, 4);
 lastResult = add(5, lastResult);
 lastResult = multiply(3,2);
-```
+
